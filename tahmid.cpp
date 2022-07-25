86ef7737664f9a0eaa8f9ddf996329fa4c2907e3
TahmidEnam <109514368+TahmidEnam@users.noreply.github.com>
2022-07-25 16:27:58 +0600

    Update tahmid.cpp

--- tahmid.cpp
+++ tahmid.cpp
@@ -1,1 +1,25 @@
-Tahj
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string S1, S2;
+
+    cout << "enter S1"<<endl;
+    cin>>S1;
+    cout << "enter S2"<<endl;
+    cin>>S2;
+   string str_inp1(S1);
+   string str_inp2(S2);
+
+cout<<"String 1:"<<str_inp1<<endl;
+cout<<"String 2:"<<str_inp2<<endl;
+int res = str_inp1.compare(str_inp2);
+
+    if (res == 0)
+        cout << "\nBoth the input strings are same." << endl;
+
+        else
+        cout<<"String are not same";
+}
+
